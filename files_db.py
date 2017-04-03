cb2e259ded4c81f487f5f51c071dcb86220a8fb8
George <gxenos95@gmail.com>
2017-04-03 15:07:39 +0300

    Version 1. Fixed basename and extension problems, added some comments.

--- files_db.py
+++ files_db.py
@@ -3,7 +3,8 @@ import math
 import hashlib
 import argparse
 
-# Function to return size from bytes
+
+# Function to return size from bytes, transformed into KB,MB,etc
 def convert_size(size_bytes):
     if size_bytes == 0:
         return '0B'
@@ -14,6 +15,7 @@ def convert_size(size_bytes):
     return '%s %s' % (round_size, size_name[size_name_pointer])
 
 
+# Returns the md5checksum of a file, reading 8KB each time, for memory saving
 def md5checksum(filepath):
     with open(filepath, 'rb') as fh:
         m = hashlib.md5()
@@ -25,23 +27,34 @@ def md5checksum(filepath):
     return m.hexdigest()
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("path_to_scan", help="The path you wish to scan.")
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path_to_scan", help="The path you wish to scan.")
+    args = parser.parse_args()
 
-for root, dirnames, files in os.walk(args.path_to_scan):
-    for file in files:
+    for root, dirnames, files in os.walk(args.path_to_scan):
+        for file in files:
 
-        info = os.stat(os.path.join(root, file))
-        print("Name: ", file)
-        print("Size: ", convert_size(info.st_size))
-        print("Path Relative to Scan Point:", os.path.relpath(root))
-        print("MD5 Hash: ", md5checksum(os.path.join(root, file)))
-        print("Basename: ", file.split(".")[0])
-        print("Extension: ", file.split(".")[1])
-        print("Directory Name: ", os.path.split(root)[1])
-        permissions = info.st_mode
-        print("Read:", bool(permissions & 0o00400), "Write:", bool(permissions & 0o00200),"Execute:", bool(permissions & 0o00100))
-        print("-------------------------")
+            info = os.stat(os.path.join(root, file))
+            print("Name: ", file)  # Prints the name of the file, with its extension
+            print("Size: ", convert_size(info.st_size))  # Prints the size of the file using convert_size()
+            print("Path Relative to Scan Point:", os.path.relpath(root))  # Prints the path of the file relative to the scan point
+            print("MD5 Hash: ", md5checksum(os.path.join(root, file)))  # Prints the md5checksum of the file using md5checksum()
+            # Prints the basename and the extension of a file
+            if "." in file:
+                print("Basename: ", file.split(".")[0])
+                print("Extension: ", ".".join(file.split(".")[1:]))  # If more than one '.' exist
+            else:
+                print("Basename: ", file)
+                print("Extension: ", "No extension")  # If there are no '.' there is no extension
+
+            print("Directory Name: ", os.path.split(root)[1])
+            permissions = info.st_mode
+            print("Read:", bool(permissions & 0o00400), "Write:", bool(permissions & 0o00200),"Execute:", bool(permissions & 0o00100))
+            print("-------------------------")
+
+
+if __name__ == "__main__":
+    main()
 
 
